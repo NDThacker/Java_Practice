66df2ff228425c1c2092ebfc1927f2f688e395c6
Naman <namanthacker1965@gmail.com>
2019-11-23 20:16:48 +0530

    few changes

--- Driver.java
+++ Driver.java
@@ -6,10 +6,17 @@ public class Driver
 {
     public static void main(String[] args)
     {
-        int fInt, sInt;
+        int fInt = 0, sInt = 0;
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		fInt = Integer.parseInt(br.readLine());
-		sInt = Integer.parseInt(br.readLine());
+		try
+		{
+			fInt = Integer.parseInt(br.readLine());
+			sInt = Integer.parseInt(br.readLine());
+		}
+		catch(IOException ex)
+		{
+			System.out.println(ex);
+		}
 
 		if(fInt > sInt) System.out.println(">");
 		else if(fInt < sInt) System.out.println("<");
